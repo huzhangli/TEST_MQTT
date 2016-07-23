63dd6b401234b6e7fc4f7f21d6c0153d04a4363b
Mikhail Tuchkov <mtuchkov@microsoft.com>
2016-07-22 18:59:43 -0700

    1

--- csharp/device/Microsoft.Azure.Devices.Client/Transport/IHttpClientHelper.cs
+++ csharp/device/Microsoft.Azure.Devices.Client/Transport/IHttpClientHelper.cs
@@ -32,6 +32,9 @@ namespace Microsoft.Azure.Devices.Client.Transport
             IDictionary<HttpStatusCode, Func<HttpResponseMessage, Task<Exception>>> errorMappingOverrides,
             CancellationToken cancellationToken) where T: IETagHolder;
 
+#if WINDOWS_UWP
+        [Windows.Foundation.Metadata.DefaultOverload]
+#endif
         Task PostAsync<T>(
             Uri requestUri, 
             T entity, 
